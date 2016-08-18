2982fbaf86364c6f89962c8e8bffb5327c9bf2b9
Nelson Tang <nelson.nt.tang@gmail.com>
2016-08-17 20:29:23 -0400

    new footer
    new footer implemented

--- js/main.js
+++ js/main.js
@@ -22,37 +22,15 @@ app.config(function($routeProvider){
 });
 
 app.controller('mainCtrl', function($scope, $timeout, $window){
-    
-//here
     /*
     var appWindow = angular.element($window);
     
     appWindow.bind('resize', function(){
         console.log('resized your browser')
     });*/
-    
-    console.log("controller ok");
+    console.log("controller okk");
+    addFooter();
 });
-
-/*
-angular.module('myApp.directives', [])
-    .directive('repeatHelloWorld', ['$timeout', function (timer) {
-    return {
-        link: function (scope, elem, attrs, ctrl) {
-            var hello = function () {
-                console.info("Repeat number: " + attrs.repeatHelloWorld);
-                for (var i = 0; i < attrs.repeatHelloWorld; i++) {
-                    console.log("Hello world!");
-                }
-            }
-            hello();
-            /* Doesn't Work! 
-            * Try:            
-             timer(hello, 0);
-            
-        }
-    }
-}]);*/
 
 app.directive("test", ['$timeout', function (timer){
     return{
